33f4d395fd202f2fb5bec87f4413594369e79262
Prudhvi Mallavarapu <mallavarapuprudhvi@gmail.com>
2024-01-27 17:41:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function downloadPDF() {
+  const pdfUrl = "./assets/Prudhvi_CV.pdf";
+
+  let link = document.createElement("a");
+  link.href = pdfUrl;
+  link.download = "Prudhvi_FullstackWebDev.pdf"; // You can change the downloaded file name here
+  link.target = "_blank";
+
+  const clickEvent = new MouseEvent("click", {
+    view: window,
+    bubbles: true,
+    cancelable: false,
+  });
+
+  link.dispatchEvent(clickEvent);
+}
